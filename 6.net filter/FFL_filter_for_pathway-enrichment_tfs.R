c2f264f94ecb1952e05e8868fedfe798d35ff8d9
Hu FeiFei <949985347@qq.com>
2018-04-11 20:00:53 +0800

    add FC info to FFL
    add FC info to FFL

--- 6.net filter/FFL_filter_for_pathway-enrichment_tfs.R
+++ 6.net filter/FFL_filter_for_pathway-enrichment_tfs.R
@@ -1,8 +1,14 @@
 FFL_data_path <- "F:/我的坚果云/ENCODE-TCGA-LUAD/通路富集/LUAD-noFC-prob0.9-kegg-gsea/FFL"
 TSG_onco_data_path <- "F:/我的坚果云/ENCODE-TCGA-LUAD/TS and oncogene source"
-
+data_path_3 <- "H:/WD Backup.swstor/MyPC/MDNkNjQ2ZjE0ZTcwNGM0Mz/Volume{3cf9130b-f942-4f48-a322-418d1c20f05f}/study/ENCODE-TCGA-LUAD/result/noiseq_no_cutoff_result"
 
 # load data ---------------------------------------------------------------
+TF_nofil <- readr::read_tsv(file.path(data_path_3,"NOISeq_DE_TF_cpm_1_noFDR")) 
+progene_nofil <- readr::read_tsv(file.path(data_path_3,"NOISeq_DE_ProGene_cpm_1_noFDR"))
+mirna_nofil <- readr::read_tsv(file.path(data_path_3,"NOISeq_DE_mirna_noFC_cpm30.mirnaid"))
+rbind(TF_nofil,progene_nofil) %>%
+  rbind(mirna_nofil) %>%
+  dplyr::rename("SYMBOL"="gene_id") -> all_gene_nofil
 
 attribute <- read.table(file.path(FFL_data_path,"attribute-net-all.txt")) %>%
   dplyr::rename("SYMBOL"="V1","gene_type"="V2","up_down"="V3") %>%
@@ -10,6 +16,8 @@ attribute <- read.table(file.path(FFL_data_path,"attribute-net-all.txt")) %>%
 network <- read.table(file.path(FFL_data_path,"network.txt")) %>%
   dplyr::rename("From"="V1","To"="V2","regulate_type"="V3") %>%
   dplyr::as.tbl()
+network %>% 
+  readr::write_tsv(file.path(FFL_data_path,"network.txt"))
 TSG <- readr::read_tsv(file.path(TSG_onco_data_path,"TSG.source_clear(at least two evidence-no confuse).tsv")) %>%
   dplyr::mutate(hallmark="TSG")
 oncogene <- readr::read_tsv(file.path(TSG_onco_data_path,"oncogene.source_clear(at least two evidence-no confuse).tsv")) %>%
@@ -26,7 +34,9 @@ TSG %>%
 
 # filter ------------------------------------------------------------------
 attribute %>%
-  dplyr::left_join(all_cancer_relate_genes,by="SYMBOL") -> attribute.hallmark
+  dplyr::left_join(all_cancer_relate_genes,by="SYMBOL") %>%
+  dplyr::left_join(all_gene_nofil,by="SYMBOL") %>%
+  dplyr::select(SYMBOL,gene_type,hallmark,log2FC) -> attribute.hallmark
 
 attribute.hallmark %>%
   readr::write_tsv(file.path(FFL_data_path,"attribute.hallmark-added.txt"))
