6cc3f3f9c265a8f684341b3c7d8fbd6d18340d70
Hu FeiFei <949985347@qq.com>
2018-04-11 14:37:43 +0800

    TSG and oncogene
    TSG and oncogene from different source combine

---
+++ tsg_collection/tsg_collection.R
@@ -0,0 +1,116 @@
+data_path <- "F:/我的坚果云/ENCODE-TCGA-LUAD/TS and oncogene source"
+
+
+# load data ----------------------------------------------------------------
+
+bushman_onco <- readr::read_tsv(file.path(data_path,"Bushman Lab","allOnco_Feb2017.tsv")) %>%
+  dplyr::mutate(source="Brushman") %>%
+  dplyr::select(symbol,geneID,source) 
+oncogene_database <- readr::read_tsv(file.path(data_path,"oncogene database","oncogene_database-all_the_human_oncogenes.txt")) %>%
+  dplyr::mutate(source="oncogene_database") %>%
+  dplyr::rename("symbol"="OncogeneName","geneID"="OncogeneID") %>%
+  dplyr::select(symbol,geneID,source) 
+sanger_census_onco <- readr::read_tsv(file.path(data_path,"Sanger Cancer gene cencus","Tier1_Census_allWed Apr 11 03_17_29 2018.tsv")) %>%
+  dplyr::filter(`Role in Cancer` %in% c("oncogene","oncogene, fusion")) %>%
+  dplyr::mutate(source="sanger_census") %>%
+  dplyr::rename("symbol"="Gene Symbol","geneID"="Entrez GeneId") %>%
+  dplyr::select(symbol,geneID,source) 
+sanger_census_TSG <- readr::read_tsv(file.path(data_path,"Sanger Cancer gene cencus","Tier1_Census_allWed Apr 11 03_17_29 2018.tsv")) %>%
+  dplyr::filter(`Role in Cancer` %in% c("TSG","TSG, fusion")) %>%
+  dplyr::mutate(source="sanger_census") %>%
+  dplyr::rename("symbol"="Gene Symbol","geneID"="Entrez GeneId") %>%
+  dplyr::select(symbol,geneID,source) 
+sanger_census_twoside <- readr::read_tsv(file.path(data_path,"Sanger Cancer gene cencus","Tier1_Census_allWed Apr 11 03_17_29 2018.tsv")) %>%
+  dplyr::filter(`Role in Cancer` %in% c("oncogene, TSG","oncogene, TSG, fusion")) %>%
+  dplyr::mutate(source="sanger_census") %>%
+  dplyr::rename("symbol"="Gene Symbol","geneID"="Entrez GeneId") %>%
+  dplyr::select(symbol,geneID,source) 
+
+Vogelstein_onco <- readr::read_tsv(file.path(data_path,"Table of oncogenes and tumor suppressor genes from Vogelstein et al. 2013","Driver genes affected by subtle mutations.txt")) %>%
+  dplyr::filter(`Classification*` %in% "Oncogene") %>%
+  dplyr::mutate(source="Vogelstein.2013") %>%
+  dplyr::rename("symbol"="Gene Symbol") %>%
+  dplyr::select(symbol,source) 
+Vogelstein_onco$symbol %>%
+  bitr(fromType = "SYMBOL",toType = "ENTREZID",OrgDb = "org.Hs.eg.db") %>%
+  dplyr::rename("symbol"="SYMBOL","geneID"="ENTREZID") %>%
+  dplyr::inner_join(Vogelstein_onco,by="symbol") -> Vogelstein_onco
+
+Vogelstein_TSG <- readr::read_tsv(file.path(data_path,"Table of oncogenes and tumor suppressor genes from Vogelstein et al. 2013","Driver genes affected by subtle mutations.txt")) %>%
+  dplyr::filter(`Classification*` %in% "TSG")  %>%
+  dplyr::mutate(source="Vogelstein.2013") %>%
+  dplyr::rename("symbol"="Gene Symbol") %>%
+  dplyr::select(symbol,source) 
+Vogelstein_TSG$symbol %>%
+  bitr(fromType = "SYMBOL",toType = "ENTREZID",OrgDb = "org.Hs.eg.db") %>%
+  dplyr::rename("symbol"="SYMBOL","geneID"="ENTREZID") %>%
+  dplyr::inner_join(Vogelstein_TSG,by="symbol") -> Vogelstein_TSG
+TSGene <- readr::read_tsv(file.path(data_path,"TSGene","all_tumor_supressor.txt"))  %>%
+  dplyr::mutate(source="TSGene") %>%
+  dplyr::rename("symbol"="GeneSymbol","geneID"="GeneID") %>%
+  dplyr::select(symbol,geneID,source) 
+UniProt_onco <- readr::read_tsv(file.path(data_path,"UniProt","uniprot-_proto+oncogene_-filtered-organism%3A_Homo+sapiens+%28Human%29+%5B--.tab")) %>%
+  dplyr::mutate(source="UniProt") %>%
+  tidyr::separate(`Entry name`,c("symbol","species"),"_") %>%
+  dplyr::select(Entry,symbol,source) 
+UniProt_TSG <- readr::read_tsv(file.path(data_path,"UniProt","uniprot-tumor+suppressor-filtered-organism%3A_Homo+sapiens+%28Human%29+%5B9606--.tab")) %>%
+  dplyr::mutate(source="UniProt") %>%
+  tidyr::separate(`Entry name`,c("symbol","species"),"_") %>%
+  dplyr::select(Entry,symbol,source) 
+
+library(clusterProfiler)
+library(org.Hs.eg.db)
+UniProt_onco$Entry %>%
+  bitr(fromType = "UNIPROT",toType = "ENTREZID",OrgDb = "org.Hs.eg.db") %>%
+  dplyr::rename("Entry"="UNIPROT","geneID"="ENTREZID") %>%
+  dplyr::inner_join(UniProt_onco,by="Entry")%>%
+  dplyr::select(-Entry,-symbol) ->  UniProt_onco.idfit
+UniProt_TSG$Entry %>%
+  bitr(fromType = "UNIPROT",toType = "ENTREZID",OrgDb = "org.Hs.eg.db") %>%
+  dplyr::rename("Entry"="UNIPROT","geneID"="ENTREZID") %>%
+  dplyr::inner_join(UniProt_TSG,by="Entry")%>%
+  dplyr::select(-Entry,-symbol) ->  UniProt_TSG.idfit
+
+
+# gene combine ------------------------------------------------------------
+
+## oncogene --------------------------------------------------------------
+bushman_onco %>%
+  rbind(oncogene_database) %>%
+  rbind(sanger_census_onco) %>%
+  rbind(Vogelstein_onco) %>%
+  dplyr::select(-symbol) %>%
+  rbind(UniProt_onco.idfit) -> all_oncogene
+## TS Gene --------------------------------------------------------------
+sanger_census_TSG %>%
+  rbind(Vogelstein_TSG) %>%
+  rbind(TSGene) %>%
+  dplyr::select(-symbol) %>%
+  rbind(UniProt_TSG.idfit)  -> all_TSG
+  
+### fileter ---------------------------------------------------------
+all_oncogene$geneID %>% table() %>% .[.>=2] %>% names() -> oncogene_at_least_2_source
+all_TSG$geneID %>% table() %>% .[.>=2] %>% names() -> TSG_at_least_2_source
+
+oncogene_at_least_2_source %>%
+  bitr(fromType = "ENTREZID",toType = "SYMBOL",OrgDb = "org.Hs.eg.db") -> oncogene_at_least_2_source
+TSG_at_least_2_source %>%
+  bitr(fromType = "ENTREZID",toType = "SYMBOL",OrgDb = "org.Hs.eg.db") -> TSG_at_least_2_source
+
+# ###source clear ---------------------------------------------------------
+
+library(plyr)
+all_oncogene %>%
+  dplyr::rename("ENTREZID"="geneID") %>%
+  dplyr::inner_join(oncogene_at_least_2_source,by="ENTREZID") %>%
+  ddply(.(ENTREZID,SYMBOL), summarize,
+        source=paste(source,collapse=",")) -> oncogene.source_clear
+oncogene.source_clear %>%
+  readr::write_tsv(file.path(data_path,"oncogene.source_clear(at least two evidence).tsv"))
+all_TSG %>%
+  dplyr::rename("ENTREZID"="geneID") %>%
+  dplyr::inner_join(TSG_at_least_2_source,by="ENTREZID") %>%
+  ddply(.(ENTREZID,SYMBOL), summarize,
+        source=paste(source,collapse=",")) -> TSG.source_clear
+TSG.source_clear %>%
+  readr::write_tsv(file.path(data_path,"TSG.source_clear(at least two evidence).tsv"))
