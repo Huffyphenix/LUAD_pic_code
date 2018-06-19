60e2c194bbd90581bf3a44edb5cdc765775799da
Hu FeiFei <949985347@qq.com>
2018-06-19 19:41:28 +0800

    add analysis
    add some analysis and edit the output pic

--- 5.correlation/EZH2_CBX2_correlation.R
+++ 5.correlation/EZH2_CBX2_correlation.R
@@ -34,45 +34,69 @@ EZH2_CBX2_exp.gather %>%
   dplyr::filter(sample_type=="TCGA Tumor") -> EZH2_CBX2_exp.gather.T
 EZH2_CBX2_exp.gather %>%
   dplyr::filter(sample_type=="TCGA Normal") -> EZH2_CBX2_exp.gather.N
+
+# Visualize your data using scatter plots
+library("ggpubr")
+ggscatter(EZH2_CBX2_exp.gather.T, x = "CBX2", y = "EZH2", 
+          add = "reg.line", conf.int = TRUE, 
+          cor.coef = TRUE, cor.method = "pearson",
+          xlab = "Miles/(US) gallon", ylab = "Weight (1000 lbs)")
+
+# Preleminary test to check the test assumptions
+shapiro.test(EZH2_CBX2_exp.gather.T$CBX2) # p-value < 0.05, don't follow a normal distribution.
+shapiro.test(EZH2_CBX2_exp.gather.T$EZH2) # p-value < 0.05, don't follow a normal distribution.
+shapiro.test(EZH2_CBX2_exp.gather.gtex$CBX2) # p-value < 0.05, don't follow a normal distribution.
+shapiro.test(EZH2_CBX2_exp.gather.gtex$EZH2) # p-value < 0.05, don't follow a normal distribution.
+shapiro.test(EZH2_CBX2_exp.gather.N$EZH2) # p-value < 0.05, don't follow a normal distribution.
+shapiro.test(EZH2_CBX2_exp.gather.N$CBX2) # p-value < 0.05, don't follow a normal distribution.
+
+#### So we need to do spearman rank correlation.
+
 # calculation -------------------------------------------------------------
 
+## EZH2 have no correlation with CBX2 in normal tissues (GTEx data)
 broom::tidy(
   cor.test(EZH2_CBX2_exp.gather.T$CBX2,EZH2_CBX2_exp.gather.T$EZH2,method = "spearman")
   ) %>% 
   dplyr::as_tibble() %>%
   dplyr::mutate(fdr=p.adjust(p.value,method = "fdr")) %>%
   dplyr::mutate(x=8,y=11,sample_type="TCGA Tumor") %>%
-  dplyr::mutate(label=paste("Spearman Cor = ",round(estimate,2)," P.value = ",round(p.value,2),"\n","n = ",nrow(EZH2_CBX2_exp.gather.T),
-                            "                                         ",sep="")) -> text.T
+  dplyr::mutate(label=paste("Spearman r = ",round(estimate,2)," p = ",signif(fdr,2),"\n","n = ",nrow(EZH2_CBX2_exp.gather.T),
+                            "                                    ",sep="")) -> text.T
 broom::tidy(
   cor.test(EZH2_CBX2_exp.gather.gtex$CBX2,EZH2_CBX2_exp.gather.gtex$EZH2,method = "spearman")
 ) %>% 
   dplyr::as_tibble() %>%
   dplyr::mutate(fdr=p.adjust(p.value,method = "fdr")) %>%
   dplyr::mutate(x=2.7,y=3,sample_type="GTEx Normal") %>%
-  dplyr::mutate(label=paste("Spearman Cor = ",round(estimate,4)," P.value = ",round(p.value,2),"\n","n = ",nrow(EZH2_CBX2_exp.gather.gtex),
-                            "                                                  ",sep="")) -> text.N
- 
+  # dplyr::mutate(top = c(-trunc(1/fdr))) %>%
+  # dplyr::mutate(bottom=round(fdr*10^(-top),2)) %>%
+  # dplyr::mutate(p.label=paste(bottom,"*10",sep="")) %>%
+  dplyr::mutate(label=paste("Spearman r = ",round(estimate,4)," p = ",signif(fdr,2),"\n","n = ",nrow(EZH2_CBX2_exp.gather.gtex),
+                            "                                  ",sep="")) -> text.N
+
+
 text.T %>%
-  rbind(text.N) ->text
+  rbind(text.N)  %>%
+  rbind(text.N.tcga)->text
 broom::tidy(
   lm(log2(EZH2_CBX2_exp.gather.T$EZH2) ~ log2(EZH2_CBX2_exp.gather.T$CBX2)) -> lm.x
 )
 plot(log2(EZH2_CBX2_exp.gather.T$EZH2),log2(EZH2_CBX2_exp.gather.T$CBX2))
 abline(lm.x)
 
-# EZH2 have no correlation with CBX2 in normal tissues (GTEx data)
+library(ggplot2)
 EZH2_CBX2_exp.gather.gtex %>%
   ggplot(aes(x=log2(EZH2),y=log2(CBX2))) +
   geom_point(color = "#00BCD4") +
-  geom_smooth(span = 0.8, se = FALSE, fullrange=TRUE, color = "#039BE5") +
+  geom_smooth(span = 0.8, se = FALSE, fullrange=TRUE, color = "#039BE5",method = "auto") +
   facet_wrap(~sample_type,scales = "free") +
   theme_bw() +
   theme(panel.border = element_blank(),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         axis.line = element_line(color = "black")
-  ) +
+) +
   geom_text(data=text.N,aes(x=x,y=y,label=label),hjust=0.5) +
   labs(
     x = "EZH2 mRNA (log2)",
@@ -80,5 +104,55 @@ EZH2_CBX2_exp.gather.gtex %>%
   ) -> p;p
 ggsave(filename = "EZH2_CBX2_GTEx_correlation.tiff",path = "F:/我的坚果云/ENCODE-TCGA-LUAD/Figure/Figure2",device = "tiff",width = 4,height = 3)
 ggsave(filename = "EZH2_CBX2_GTEx_correlation.pdf",path = "F:/我的坚果云/ENCODE-TCGA-LUAD/Figure/Figure2",device = "pdf",width = 4,height = 3)
+
+# EZH2 have correlation with CBX2 in tumor tissues (TCGA data)
+EZH2_CBX2_exp.gather.T %>%
+  ggplot(aes(x=log2(EZH2),y=log2(CBX2))) +
+  geom_point(color = c("#FF6A6A")) +
+  geom_smooth(span = 0.8, se = FALSE, fullrange=TRUE, color = "#039BE5",method = "auto") +
+  facet_wrap(~sample_type,scales = "free") +
+  theme_bw() +
+  theme(panel.border = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.line = element_line(color = "black")
+  ) +
+  geom_text(data=text.T,aes(x=x,y=y,label=label),hjust=0.5) +
+  labs(
+    x = "EZH2 mRNA (log2)",
+    y = "CBX2 mRNA (log2)"
+  ) -> p;p
 ggsave(filename = "EZH2_CBX2_mRNA_correlation.tiff",path = "F:/我的坚果云/ENCODE-TCGA-LUAD/Figure/Figure2",device = "tiff",width = 4,height = 3)
 ggsave(filename = "EZH2_CBX2_mRNA_correlation.pdf",path = "F:/我的坚果云/ENCODE-TCGA-LUAD/Figure/Figure2",device = "pdf",width = 4,height = 3)
+
+# EZH2 dont have correlation with CBX2 in normal tissues (TCGA data)
+broom::tidy(
+  cor.test(EZH2_CBX2_exp.gather.N$CBX2,EZH2_CBX2_exp.gather.N$EZH2,method = "spearman")
+) %>% 
+  dplyr::as_tibble() %>%
+  dplyr::mutate(fdr=p.adjust(p.value,method = "fdr")) %>%
+  dplyr::mutate(x=6,y=7.5,sample_type="TCGA Normal") %>%
+  # dplyr::mutate(top = c(-trunc(1/fdr))) %>%
+  # dplyr::mutate(bottom=round(fdr*10^(-top),2)) %>%
+  # dplyr::mutate(p.label=paste(bottom,"*10",sep="")) %>%
+  dplyr::mutate(label=paste("Spearman r = ",round(estimate,4)," p = ",signif(fdr,2),"\n","n = ",nrow(EZH2_CBX2_exp.gather.N),
+                            "                                    ",sep="")) -> text.N.tcga
+
+EZH2_CBX2_exp.gather.N %>%
+  ggplot(aes(x=log2(EZH2),y=log2(CBX2))) +
+  geom_point(color = c("#00BCD4")) +
+  geom_smooth(span = 0.8, se = FALSE, fullrange=TRUE, color = "#039BE5",method = "auto") +
+  facet_wrap(~sample_type,scales = "free") +
+  theme_bw() +
+  theme(panel.border = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.line = element_line(color = "black")
+  ) +
+  geom_text(data=text.N.tcga,aes(x=x,y=y,label=label),hjust=0.5) +
+  labs(
+    x = "EZH2 mRNA (log2)",
+    y = "CBX2 mRNA (log2)"
+  ) -> p;p
+ggsave(filename = "EZH2_CBX2_tcga_normal_correlation.tiff",path = "F:/我的坚果云/ENCODE-TCGA-LUAD/Figure/Figure2",device = "tiff",width = 4,height = 3)
+ggsave(filename = "EZH2_CBX2_tcga_normal_correlation.pdf",path = "F:/我的坚果云/ENCODE-TCGA-LUAD/Figure/Figure2",device = "pdf",width = 4,height = 3)
