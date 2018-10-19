8d8f5f06ed8babdd0e7b8797dd47a5c9d432212d
Hu FeiFei <949985347@qq.com>
2018-10-19 20:04:12 +0800

    pic style changed

--- 9.cnv/EZH2_CBX2_cnv.R
+++ 9.cnv/EZH2_CBX2_cnv.R
@@ -1,5 +1,10 @@
+# E zhou path 
 data_path<- "H:/data/GSCALite/TCGA/cnv"
 result_path <- "F:/我的坚果云/ENCODE-TCGA-LUAD/Figure"
+
+# HUST path
+data_path<- "G:/data/GSCALite/TCGA/cnv"
+result_path <- "S:/坚果云/我的坚果云/ENCODE-TCGA-LUAD/Figure"
 
 library(magrittr)
 library(ggplot2)
@@ -24,19 +29,21 @@ luad_cnv %>%
 
 plot_ready %>%
   ggplot(aes(x=symbol,y=Percent,fill=type)) +
-  geom_col(position = "stack") +
+  geom_col(position = "stack", width = 0.5) +
   guides(fill=guide_legend(title = "")) +
   scale_fill_manual(values=c("#FF0000", "#0000FF"))+
-  ylab("Percent (%)") +
+  scale_y_continuous(limits = c(0,5)) +
+  ylab("CNV Percent (%)") +
+  coord_flip() +
   theme(
     panel.background = element_blank(),
     panel.border = element_rect(fill='transparent',colour = "black"),
-    legend.position = c(0.8,0.9),
+    legend.position = c(0.8,0.8),
     legend.background = element_blank(),
-    axis.title.x = element_blank(),
     axis.title = element_text(size=14),
-    axis.text = element_text(size = 12)
+    axis.title.y = element_blank(),
+    axis.text = element_text(size = 20, colour = "black")
   ) ->p;p
-ggsave(filename = "EZH2_CBX2_CNV_percent.pdf",path = result_path,device = "pdf",width = 4,height = 4)
-ggsave(filename = "EZH2_CBX2_CNV_percent.tiff",path = result_path,device = "tiff",width = 4,height = 4)
+ggsave(file.path(result_path,"Figure2","EZH2_CBX2_CNV_percent.pdf"),device = "pdf",width = 5,height = 3)
+ggsave(file.path(result_path,"Figure2","EZH2_CBX2_CNV_percent.tiff"),device = "tiff",width = 5,height = 3)
 
